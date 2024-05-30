8501463f2c36f362c7c5ea799876610433ecf39e
FocusFine89 <niivanov02@gmail.com>
2024-05-30 14:44:10 +0200

    setup

---
+++ src/main/java/NikitaIvanov/Application.java
@@ -0,0 +1,8 @@
+package NikitaIvanov;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/NikitaIvanov/ApplicationTest.java
@@ -0,0 +1,13 @@
+package NikitaIvanov;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApplicationTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
